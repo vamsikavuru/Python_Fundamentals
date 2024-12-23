2d8302086432f6b06618479248ba98e6914f66cd
LENOVO <vamsi0110@outlook.com>
2024-12-23 23:45:28 +0530

    2nd_commit

---
+++ data_types.py
@@ -0,0 +1,21 @@
+# Variables
+
+var = 234
+
+print(var)
+
+print(id(var))
+
+var_1 = 234
+
+print(id(var))
+
+a  = 11
+print(a)
+print(id(a))
+a = 10
+print(a)
+print(id(a))
+
+import keyword
+print(keyword.kwlist)
